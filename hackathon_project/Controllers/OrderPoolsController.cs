4ff865663a91c8df1f88d0bb4ed5eb9bda265893
Ebrar Elest Sen <ebrarelestsen03@gmail.com>
2026-05-17 18:49:40 +0300

    Sipariş havuzları eklendi

---
+++ hackathon_project/Controllers/OrderPoolsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using hackathon_project.Data;
+
+namespace hackathon_project.Controllers
+{
+    public class OrderPoolsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderPoolsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var pools = await _context.OrderPools
+                .Include(x => x.Restaurant)
+                .ToListAsync();
+
+            return View(pools);
+        }
+    }
+}
---
+++ hackathon_project/Models/OrderPool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace hackathon_project.Models;
+
+public partial class OrderPool
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(150)]
+    public string Title { get; set; } = null!;
+
+    public int RestaurantId { get; set; }
+
+    [StringLength(100)]
+    public string Dormitory { get; set; } = null!;
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal CurrentAmount { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime EndTime { get; set; }
+
+    [StringLength(50)]
+    public string Status { get; set; } = null!;
+
+    [InverseProperty("OrderPool")]
+    public virtual ICollection<Participant> Participants { get; set; } = new List<Participant>();
+
+    [ForeignKey("RestaurantId")]
+    [InverseProperty("OrderPools")]
+    public virtual Restaurant Restaurant { get; set; } = null!;
+}
